f6c6592a9b6e37f9e18299dfcd8b2d1981c4063f
kodjobaah <your_github_email>
2025-06-17 18:33:52 +0100

    fix import issues

--- llava/model/__init__.py
+++ llava/model/__init__.py
@@ -1,21 +1,7 @@
 import os
+from .language_model.llava_llama import LlavaLlamaForCausalLM, LlavaConfig
+from .language_model.llava_qwen import LlavaQwenForCausalLM, LlavaQwenConfig
+from .language_model.llava_mistral import LlavaMistralForCausalLM, LlavaMistralConfig
+from .language_model.llava_mixtral import LlavaMixtralForCausalLM, LlavaMixtralConfig
+from .language_model.llava_mamba import LlavaMambaModel, LlavaMambaConfig
 
-AVAILABLE_MODELS = {
-    "llava_llama": "LlavaLlamaForCausalLM, LlavaConfig",
-    "llava_qwen": "LlavaQwenForCausalLM, LlavaQwenConfig",
-    "llava_mistral": "LlavaMistralForCausalLM, LlavaMistralConfig",
-    "llava_mixtral": "LlavaMixtralForCausalLM, LlavaMixtralConfig",
-    # "llava_qwen_moe": "LlavaQwenMoeForCausalLM, LlavaQwenMoeConfig",    
-    "llava_mamba" : "LlavaMambaModel, LlavaMambaConfig",
-    
-    # Add other models as needed
-}
-
-__all__ = []
-
-for model_name, model_classes in AVAILABLE_MODELS.items():
-    try:
-        exec(f"from .language_model.{model_name} import {model_classes}")
-        __all__.extend(model_classes.replace(" ", "").split(","))
-    except Exception as e:
-        print(f"Failed to import {model_name} from llava.language_model.{model_name}. Error: {e}")
