38a1fab37de2b70c40418c4d952dc2d75835fce3
kodjobaah <your_github_email>
2025-06-17 18:18:02 +0100

    fix import issues

--- llava/model/__init__.py
+++ llava/model/__init__.py
@@ -6,11 +6,16 @@ AVAILABLE_MODELS = {
     "llava_mistral": "LlavaMistralForCausalLM, LlavaMistralConfig",
     "llava_mixtral": "LlavaMixtralForCausalLM, LlavaMixtralConfig",
     # "llava_qwen_moe": "LlavaQwenMoeForCausalLM, LlavaQwenMoeConfig",    
+    "llava_mamba" : "LlavaMambaModel, LlavaMambaConfig",
+    
     # Add other models as needed
 }
+
+__all__ = []
 
 for model_name, model_classes in AVAILABLE_MODELS.items():
     try:
         exec(f"from .language_model.{model_name} import {model_classes}")
+        __all__.extend(model_classes.replace(" ", "").split(","))
     except Exception as e:
         print(f"Failed to import {model_name} from llava.language_model.{model_name}. Error: {e}")
